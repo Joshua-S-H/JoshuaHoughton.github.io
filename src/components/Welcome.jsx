d2a920cd04a349132c711f09f407188451b55bfb
Joshua Houghton <joshua.s.houghton@gmail.com>
2024-12-10 19:42:53 +0000

    This rejiging

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,9 +1,9 @@
-import './HeaderStyle.css'
+import './WelcomeStyle.css'
 
 export function Welcome(){
     return (
         <>  
-            <div>
+            <div className="welcome">
                 <h1>Welcome!</h1>
                 <h4>Hi, this page is a fun project to showcase some of my personal software engineering projects.</h4>
                 <h4>I am currently looking for software engineering and I you are interested in having a talk about a possible position please drop me a message on Linked in or send me and email and we can discuss things further!</h4>
