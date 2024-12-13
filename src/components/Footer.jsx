c4f563f78cb5370e6a50c63c23b7a4f07f6a9026
Joshua Houghton <joshua.s.houghton@gmail.com>
2024-12-13 11:23:06 +0000

    Complete Version 1.0

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export function Footer(){
         <>
         <footer>
            
-            <div id="projects" className="footer-section">
+            <div className="footer-section">
                 <div className="about-section">
                     {/* <h3>About</h3> */}
                     {/* <p>Write this about. Open to work</p> */}
@@ -15,13 +15,12 @@ export function Footer(){
                     <div className="socials-section">
                         <h3>Contacts | </h3>
                         <div className="social-icons-container">
-                            <a href="https://www.linkedin.com/in/joshua-houghton-b1b9061a6/"><img src="src/assets/dark_linkedin.png" className="icon-style" alt="Github icon"  /></a>
-                            <a href="https://github.com/Joshua-S-H"><img src="src/assets/github.jpeg" className="icon-style" alt="Github icon"  /></a>
-                            <a href="mailto:joshua.s.houghton@gmail.com"><img src="src/assets/email.jpeg" className="icon-style" alt="Github icon"  /></a>
+                            <a href="https://www.linkedin.com/in/joshua-houghton-b1b9061a6/" target="_blank" ><img src="src/assets/dark_linkedin.png" className="icon-style" alt="Github icon"  /></a>
+                            <a target="_blank" href="https://github.com/Joshua-S-H"><img src="src/assets/github.jpeg" className="icon-style" alt="Github icon"  /></a>
+                            <a target="_blank" href="mailto:joshua.s.houghton@gmail.com"><img src="src/assets/email.jpeg" className="icon-style" alt="Github icon"  /></a>
                         </div>
                     </div>
                 </div>
-                
             </div>
         </footer>
         </>
--- src/components/Who.jsx
+++ src/components/Who.jsx
@@ -4,12 +4,20 @@ export function Who() {
   return (
     <>
       <div className="who">
-        <h2>Who Am I?</h2>
-        <p>I'm 22 year old software engineer / data scientist biased in London <br></br>
-        Warwick Uni MEng<br></br>
-        Love Tennis, Running and doing cool stuff with data<br></br>
-        Insert a number of photo of me to to the right
-        </p>
+        <div className="whoText">
+          <h2>Who Am I?</h2>
+          <p>I'm 22 year old software / data engineer based in London <br></br>
+          Masters in Systems engineering from the University of Warwick MEng<br></br>
+          I Love lacrosse, running and doing cool stuff with data<br></br>
+          Feel free to drop me a message on LinkedIn
+          </p>
+        </div>
+        <div class="photo-collage">
+          <img class="HeroTeam-Photo" src="src/assets/photos_of_me/HeroGroup.JPG" alt="HeroTeam Photo"  />
+          <img class="HeadShot-Photo" src="src/assets/photos_of_me/Headshot.JPG" alt="HeadShot" />
+          {/* <img class="photo photo1" src="src/assets/photos_of_me/GPTeam.JPG" alt="Photo of me" /> */}
+          <img class="LacrosseTeam-Photo" src="src/assets/photos_of_me/lacrosseTeam.JPG" alt="LacrosseTeam Photo" />
+        </div>
       </div>
     </>
   );
